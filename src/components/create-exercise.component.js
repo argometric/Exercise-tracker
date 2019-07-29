48317c781c5bc90b8c16e2905d34c91429d0f1e2
armin schweighofer <arminschweighofer@hotmail.com>
2019-07-29 12:41:53 +0200

    Add User + Add Exercise backend + DB functionality

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import DatePicker from 'react-datepicker';
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -22,27 +23,35 @@ export default class CreateUser extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ["test user"],
-      username: "test user"
-    });
-  }
+    axios.get('http://localhost:5000/users/')
+    .then(response => {
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map(user => user.username),
+          username: response.data[0].username
+        });
+      }
+  })
+}
 
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
     });
   }
+
   onChangeDescription(e) {
     this.setState({
       description: e.target.value
     });
   }
+
   onChangeDuration(e) {
     this.setState({
       duration: e.target.value
     });
   }
+
   onChangeDate(date) {
     this.setState({
       date: date
@@ -60,6 +69,10 @@ export default class CreateUser extends Component {
     };
 
     console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/add', exercise)
+    .then(res => console.log(res.data));
+
 
     window.location = "/";
   }
@@ -125,6 +138,6 @@ export default class CreateUser extends Component {
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
